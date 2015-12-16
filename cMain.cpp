9192887264112ebad8e63a7690e2ef4b8d267cc1
DavidFisher201 <davidfisher724@gmail.com>
2015-12-16 16:35:16 +0000

    Create cMain.cpp

---
+++ cMain.cpp
@@ -0,0 +1,138 @@
+#define WIN32_LEAN_AND_MEAN
+#define WIN32_EXTRA_LEAN
+
+#define GLX_GLXEXT_LEGACY //Must be declared so that our local glxext.h is picked up, rather than the system one
+
+
+#include <windows.h>
+#include "windowOGL.h"
+#include "GameConstants.h"
+#include "cWNDManager.h"
+#include "cInputMgr.h"
+#include "cSoundMgr.h"
+#include "cFontMgr.h"
+#include "cSprite.h"
+#include "courseworkGame.h"
+
+int WINAPI WinMain(HINSTANCE hInstance,
+                   HINSTANCE hPrevInstance,
+                   LPSTR cmdLine,
+                   int cmdShow)
+{
+
+    //Set our window settings
+    const int windowWidth = 1024;
+    const int windowHeight = 768;
+    const int windowBPP = 16;
+
+
+
+    //This is our window
+	static cWNDManager* pgmWNDMgr = cWNDManager::getInstance();
+
+	// This is the input manager
+	static cInputMgr* theInputMgr = cInputMgr::getInstance();
+
+	// This is the sound manager
+	static cSoundMgr* theSoundMgr = cSoundMgr::getInstance();
+
+	// This is the Font manager
+	static cFontMgr* theFontMgr = cFontMgr::getInstance();
+
+	//The example OpenGL code
+    windowOGL theOGLWnd;
+	
+    //Attach our the OpenGL window
+	pgmWNDMgr->attachOGLWnd(&theOGLWnd);
+
+	// Attach the keyboard manager
+	pgmWNDMgr->attachInputMgr(theInputMgr);
+
+    //Attempt to create the window
+	if (!pgmWNDMgr->createWND(windowWidth, windowHeight, windowBPP))
+    {
+        //If it fails
+
+        MessageBox(NULL, "Unable to create the OpenGL Window", "An error occurred", MB_ICONERROR | MB_OK);
+		pgmWNDMgr->destroyWND(); //Reset the display and exit
+        return 1;
+    }
+
+	if (!theOGLWnd.initOGL(windowWidth, windowHeight)) //Initialize our example
+    {
+        MessageBox(NULL, "Could not initialize the application", "An error occurred", MB_ICONERROR | MB_OK);
+		pgmWNDMgr->destroyWND(); //Reset the display and exit
+        return 1;
+    }
+
+	//Clear key buffers
+	theInputMgr->clearBuffers(theInputMgr->KEYS_DOWN_BUFFER | theInputMgr->KEYS_PRESSED_BUFFER);
+
+	/* initialize random seed: */
+	srand((unsigned int)time(NULL));
+
+	
+
+	
+	// Load Sound
+	LPCSTR gameSounds[3] = { "Audio/Background_Song.wav", "Audio/Laser.wav"};
+
+	//theSoundMgr->add("Theme", gameSounds[0]);
+	theSoundMgr->add("Shot", gameSounds[1]);
+	theSoundMgr->add("Explosion", gameSounds[2]);
+
+	// Load Fonts
+	LPCSTR gameFonts[2] = { "Fonts/digital-7.ttf", "Fonts/space age.ttf" };
+
+	theFontMgr->addFont("SevenSeg", gameFonts[0], 24);
+	theFontMgr->addFont("Space", gameFonts[1], 24);
+
+	
+	//Create the background
+	cTexture textureBkgd;
+	textureBkgd.createTexture("Images\\space_background.png");
+	cBkGround spriteBkgd;
+	spriteBkgd.setSpritePos(glm::vec2(0.0f, 0.0f));
+	spriteBkgd.setTexture(textureBkgd.getTexture());
+	spriteBkgd.setTextureDimensions(textureBkgd.getTWidth(), textureBkgd.getTHeight());
+
+	//Create the rocket sprite
+	cTexture rocketTxt;
+	rocketTxt.createTexture("Images\\rocket.png");
+	cRocket rocketSprite;
+	rocketSprite.attachInputMgr(theInputMgr); // Attach the input manager to the sprite
+	rocketSprite.setSpritePos(glm::vec2(112.0f, 380.0f));
+	rocketSprite.setTexture(rocketTxt.getTexture());
+	rocketSprite.setTextureDimensions(rocketTxt.getTWidth(), rocketTxt.getTHeight());
+	rocketSprite.setSpriteCentre();
+	rocketSprite.setRocketVelocity(glm::vec2(0.0f, 0.0f));
+
+	// Attach sound manager to rocket sprite
+	rocketSprite.attachSoundMgr(theSoundMgr);
+
+    //This is the mainloop, we render frames until isRunning returns false
+	while (pgmWNDMgr->isWNDRunning())
+    {
+		pgmWNDMgr->processWNDEvents(); //Process any window events
+
+        //We get the time that passed since the last frame
+		float elapsedTime = pgmWNDMgr->getElapsedSeconds();
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		spriteBkgd.render();
+
+		rocketSprite.update(elapsedTime);
+
+		
+		rocketSprite.render();
+		
+		//Clear the key buffers
+		pgmWNDMgr->swapBuffers();
+		theInputMgr->clearBuffers(theInputMgr->KEYS_DOWN_BUFFER | theInputMgr->KEYS_PRESSED_BUFFER);
+    }
+
+	theOGLWnd.shutdown(); //Free any resources
+	pgmWNDMgr->destroyWND(); //Destroy the program window
+
+    return 0; //Return success
+}
